42de495ff0b45676cc6c95648513935a5f5a4ae0
javierAraluce <javier.araluce@uah.es>
2021-06-17 17:24:53 +0200

    launch

--- src/launch_scenario_test.sh
+++ src/launch_scenario_test.sh
@@ -1,5 +1,12 @@
+# echo 'Scenario'
+# sleep 2
+# python3 scenario_runner.py --openscenario t4ac_scenarios/t4ac_ACC_CCRM.xosc --sync &
+# sleep 6 
+# python3 manual_control_full_sensors.py -a -t 3 --res 5760x1080
+
+
 echo 'Scenario'
 sleep 2
-python3 scenario_runner.py --route data_leaderboard/routes_devtest.xml data_leaderboard/all_towns_traffic_scenarios_public.json 1  --reloadWorld --sync --debug &
+python3 scenario_runner.py --openscenario t4ac_scenarios/t4ac_Overtaking_CCRM.xosc --sync &
 sleep 6 
-python3 manual_control_full_sensors.py 
+python3 manual_control_full_sensors.py -a -t 3 --res 5760x1080
